949d17e60b190dc89cb35e14927cfcc660dc12e9
dikshantgh <dikkughimire@gmail.com>
2017-08-14 01:35:27 +0545

    changed urls as per class

--- poll/urls.py
+++ poll/urls.py
@@ -5,7 +5,10 @@ app_name ='poll'
 urlpatterns = [
     url(r"^$", views.QuestionTypeView.as_view(), name="main_page"),
     url(r"^questions/(?P<pk>\d+)/$", views.QuestionBriefView.as_view(), name="question_list"),
-    url(r"^option/(?P<pk>\d+)/$", views.QuestionDetailView.as_view(), name="question_detail")
+    url(r"^option/(?P<pk>\d+)/$", views.QuestionDetailView.as_view(), name="question_detail"),
+    url(r"^type/$", views.TypeCreateView.as_view(), name="create_type"),
+    url(r"^createquestion/(?P<pk>\d+)/$", views.QuestionCreateView.as_view(), name="create_question"),
+
 
 
 
--- poll/views.py
+++ poll/views.py
@@ -32,10 +32,28 @@ class QuestionDetailView(DetailView):
     model = Question
     template_name = "poll/question_detail.html"
     form_class = QuestionForm
-    def get_context_data(self, **kwargs):
-        context = super(QuestionDetailView, self).get_context_data(**kwargs)
-        movie_id = self.kwargs['pk']
-        context['page'] = Question.objects.get(id =movie_id)
-        return context
+    
+
+class TypeCreateView(CreateView):
 
 
+    form_class = QuestionTypeForm
+    template_name = "poll/create_type.html"
+    success_url = reverse_lazy('poll:main_page')
+
+
+class QuestionCreateView(CreateView):
+
+
+    form_class = QuestionForm
+    template_name = "poll/create_question.html"
+    success_url = reverse_lazy('poll:main_page')
+
+    def get_initial(self):
+        initials = super(QuestionCreateView, self).get_initial()
+        initials['question_type'] = self.kwargs['pk']
+        return initials
+
+
+    
+    
